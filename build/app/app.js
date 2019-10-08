6b82e3f9fd668793190a40bf88dac6d1ec577553
MPink <menatpink@gmail.com>
2019-10-08 03:27:12 +0100

    addition of Interactiveness

--- build/app/app.js
+++ build/app/app.js
@@ -1,1 +1,1 @@
-"use strict";var myName="Menats",hello=function(){return"hello ".concat(myName)};
+"use strict";var burgerMenu=document.querySelector("#nav-opener"),openNavigation=document.querySelector(".full-screen-navigation");burgerMenu.addEventListener("click",function(){openNavigation.classList.remove("nav-close")}),openNavigation.addEventListener("click",function(e){e.currentTarget.classList.add("nav-close")});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 // Menu Nav Open Event
+const burgerMenu = document.querySelector('#nav-opener');
+const openNavigation = document.querySelector('.full-screen-navigation');
+burgerMenu.addEventListener('click', () => {
+    openNavigation.classList.remove('nav-close');
+});
+//Close
+openNavigation.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('nav-close')
+});
