fa2f450ba5f9cee67e29d73354385f1f9f13a938
MPink <menatpink@gmail.com>
2019-10-16 06:59:43 +0100

    Additional changes to structrue responsiveness and interactivity

--- build/app/all.js
+++ build/app/all.js
@@ -11,3 +11,17 @@ burgerMenu.click(function () {
 // openNavigation.click(function (e) {
 //     e.currentTarget.addClass('nav-close')
 // });
+$(document).ready(function () {
+    $('.carousel-container').slick({
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 300,
+        appendArrows: $('.about-name'),
+        prevArrow: '<button type="button" class="slick-prev"><</button>',
+        nextArrow: '<button type="button" class="slick-next">></button>',
+    })
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require("gulp"),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
     babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
     config = {
         paths: {
             html: {
@@ -33,12 +35,22 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest(config.paths.html.dest))
 });
 
-gulp.task('uglify-js', () => {
+gulp.task('minify-js', () => {
     return gulp.src(config.paths.javascript.src)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(config.paths.javascript.dest))
+        .pipe(rename('uglify.js'))
         .pipe(babel({ presets: ['@babel/preset-env'] }))
         .pipe(uglify())
         .pipe(gulp.dest(config.paths.javascript.dest))
 });
+
+// gulp.task('uglify-js', () => {
+//     return gulp.src('build/app/all.js')
+//         .pipe(babel({ presets: ['@babel/preset-env'] }))
+//         .pipe(uglify())
+//         .pipe(gulp.dest(config.paths.javascript.dest))
+// });
 
 gulp.task('sass', () => {
     return gulp.src(config.paths.sass.src)
@@ -47,10 +59,10 @@ gulp.task('sass', () => {
         .pipe(gulp.dest(config.paths.sass.dest))
 });
 
-gulp.task("build", gulp.series('minify-html', 'uglify-js', 'sass'))
+gulp.task("build", gulp.series('minify-html', 'minify-js', 'sass'))
 
 gulp.task("default", () => {
     gulp.watch(config.paths.sass.src, gulp.series("sass"));
-    gulp.watch(config.paths.javascript.src, gulp.series("uglify-js")); 
+    gulp.watch(config.paths.javascript.src, gulp.series("minify-js"));
 });
 
